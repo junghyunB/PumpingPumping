eac881455f851c8ca2de51d716011adbdb7fdf75
junghyunB <bjh940126@gmail.com>
2022-10-28 18:20:56 +0900

    redux 추가 작업 중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,85 +1,94 @@
 import { useEffect, useState } from "react";
 // import Caver from "caver-js";
 import { caver, raffleV1Contract, RAFFLEV1_CONTRACT_ADDRESS } from "./caverConfig";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { connectAccount } from './redux/actions/connectAccount' 
 function App() {
 
-  const decimals = 10 ** 18;
-  const [accounts, setAccount] = useState("");  
-  const [totalAmounts, setTotalAmount] = useState(0);
-  const [investAmount, setInvestAmount] = useState(0);
-  const [inputvalue, setInpubValue] = useState(0);
-  const [winner, setWinner] = useState("");
-  const myratio = investAmount / totalAmounts * 100;
+  const dispatch = useDispatch();
+  const {accounts} = useSelector(state => state.account);
+  console.log(accounts)
+  // const decimals = 10 ** 18;
 
-  const connectKaikas = async () => {
-    try {
-    const account = await window.klaytn.enable();
-    setAccount(account[0]);
-    } catch(error) {
-      console.error(error);
-    }
-  };
+  // const [totalAmounts, setTotalAmount] = useState(0);
+  // const [investAmount, setInvestAmount] = useState(0);
+  // const [inputvalue, setInpubValue] = useState(0);
+  // const [winner, setWinner] = useState("");
+  // const myratio = investAmount / totalAmounts * 100;
+  // console.log(accounts);
 
+  // const connectKaikas = async () => {
+  //   try {
+  //   const account = await window.klaytn.enable();
+  //   setAccount(account[0]);
+  //   } catch(error) {
+  //     console.error(error);
+  //   }
+  // };
+  const connectWallet = () => {
+    if(window.klaytn)
+    dispatch(connectAccount.getAccount())
+}
 
-  const applyToRaffle = async() => {
-    const response = await caver.klay.sendTransaction({
-      from:accounts,
-      to: RAFFLEV1_CONTRACT_ADDRESS,
-      value: caver.utils.convertToPeb(inputvalue, "KLAY"),
-      gas: "3000000",
-      data: raffleV1Contract.methods.applyToRaffle(inputvalue).encodeABI()
-    })
-  }
+  // const applyToRaffle = async() => {
+  //   const response = await caver.klay.sendTransaction({
+  //     from:accounts,
+  //     to: RAFFLEV1_CONTRACT_ADDRESS,
+  //     value: caver.utils.convertToPeb(inputvalue, "KLAY"),
+  //     gas: "3000000",
+  //     data: raffleV1Contract.methods.applyToRaffle(inputvalue).encodeABI()
+  //   })
+  // }
   
-  const totalAmount = async() => {
-     const result = await raffleV1Contract.methods.totalAmount().call();
-     setTotalAmount(result / decimals);
-  }
+  // const totalAmount = async() => {
+  //    const result = await raffleV1Contract.methods.totalAmount().call();
+  //    setTotalAmount(result / decimals);
+  // }
 
-  const getRatio = async() => {
-    const response = await raffleV1Contract.methods.getInvestAmount(accounts).call();
-    setInvestAmount(response);
-  }
+  // const getRatio = async() => {
+  //   const response = await raffleV1Contract.methods.getInvestAmount(accounts).call();
+  //   setInvestAmount(response);
+  // }
 
-  const winnerOfRaffle = async() => {
-    const response = await caver.klay.sendTransaction({
-      from:accounts,
-      to: RAFFLEV1_CONTRACT_ADDRESS,
-      gas: "3000000",
-      data: raffleV1Contract.methods.winnerOfRaffle().encodeABI()
-    })
-  }
+  // const winnerOfRaffle = async() => {
+  //   const response = await caver.klay.sendTransaction({
+  //     from:accounts,
+  //     to: RAFFLEV1_CONTRACT_ADDRESS,
+  //     gas: "3000000",
+  //     data: raffleV1Contract.methods.winnerOfRaffle().encodeABI()
+  //   })
+  // }
 
-  const rewardTransfer = async() => {
-    const response = await caver.klay.sendTransaction({
-      from:accounts,
-      to: RAFFLEV1_CONTRACT_ADDRESS,
-      gas: "3000000",
-      data: raffleV1Contract.methods.rewardTransfer().encodeABI()
-    })
-  }
-  const getWinner = async() => {
-    const res = await raffleV1Contract.methods.getWinner().call();
-    setWinner(res);
-  }
+  // const rewardTransfer = async() => {
+  //   const response = await caver.klay.sendTransaction({
+  //     from:accounts,
+  //     to: RAFFLEV1_CONTRACT_ADDRESS,
+  //     gas: "3000000",
+  //     data: raffleV1Contract.methods.rewardTransfer().encodeABI()
+  //   })
+  // }
+  // const getWinner = async() => {
+  //   const res = await raffleV1Contract.methods.getWinner().call();
+  //   setWinner(res);
+  // }
 
 
-  console.log(winner)
+
   
   useEffect(() => {
-    connectKaikas()
-    totalAmount()
-    getRatio()
-    getWinner()
-  },[accounts])
+    // totalAmount()
+    // getRatio()
+    // getWinner()
+    dispatch(connectAccount.getAccount())
+  },[])
   return (
     <div>
-      <button onClick={connectKaikas}>ConnectKaikas</button>
+      {/* <button onClick={connectKaikas}>ConnectKaikas</button> */}
+      <button onClick={connectWallet}>ConnectKaikas</button>
       <br />
       <p>연결된 주소 : {accounts}</p>
       <br />
-      <p>현재 총 모금액 : {totalAmounts} KLAY</p>
+      {/* <p>현재 총 모금액 : {totalAmounts} KLAY</p>
     
       <p>내 당첨 확률 : {investAmount == 0 ? 0 : myratio} %</p>
       <br /> 
@@ -94,7 +103,7 @@ function App() {
       <br /> 
       <p>우승자 : {winner}</p>
       <br /> 
-      <button onClick={rewardTransfer}>우승 상금 전송</button>
+      <button onClick={rewardTransfer}>우승 상금 전송</button> */}
     </div>
   );
 }
